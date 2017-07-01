8281a1821f1f45b032008727a1095c6c61700c6a
Guannan Wei <kiss.kraks@gmail.com>
2017-07-01 16:57:13 -0700

    Add STLC-Sub and System F

---
+++ stlc-sub.rkt
@@ -0,0 +1,245 @@
+#lang racket
+
+;; Simple Typed Lamdba Calculus with Record and Subtyping
+
+(require rackunit)
+
+;; Expressions
+
+(struct NumE (n) #:transparent)
+(struct BoolE (b) #:transparent)
+(struct IdE (id) #:transparent)
+(struct PlusE (l r) #:transparent)
+(struct MultE (l r) #:transparent)
+(struct AppE (fun arg) #:transparent)
+(struct LamE (arg arg-type body) #:transparent)
+(struct IfE (cnd thn els) #:transparent)
+
+(struct RecordE (ns es) #:transparent)
+(struct GetE (rec n) #:transparent)
+(struct SetE (rec n val) #:transparent)
+
+;; Types
+
+(struct NumT () #:transparent)
+(struct BoolT () #:transparent)
+(struct ArrowT (arg res) #:transparent)
+(struct RecordT (ns ts) #:transparent)
+
+;; Values
+
+(struct NumV (n) #:transparent)
+(struct BoolV (b) #:transparent)
+(struct RecordV (ns vs) #:transparent)
+(struct ClosureV (arg body env) #:transparent)
+
+;; Environment & Type Environment
+
+(define (make-lookup error-hint isa? name-of val-of)
+  (λ (name vals)
+    (cond [(empty? vals) (error error-hint "free variable")]
+          [(and (isa? (first vals))
+                (equal? name (name-of (first vals))))
+           (val-of (first vals))]
+          [else ((make-lookup error-hint isa? name-of val-of) name (rest vals))])))
+
+(struct Binding (name val) #:transparent)
+(define lookup (make-lookup 'lookup Binding? Binding-name Binding-val))
+(define ext-env cons)
+
+(struct TypeBinding (name type) #:transparent)
+(define type-lookup (make-lookup 'type-lookup TypeBinding? TypeBinding-name TypeBinding-type))
+(define ext-tenv cons)
+
+;; Parser
+
+(define (parse s)
+  (match s
+    [(? number? x) (NumE x)]
+        ['true (BoolE #t)]
+    ['false (BoolE #f)]
+    [(? symbol? x) (IdE x)]
+    [`(+ ,l ,r) (PlusE (parse l) (parse r))]
+    [`(* ,l ,r) (MultE (parse l) (parse r))]
+    [`(rec {,ns : ,vs} ...)
+     (RecordE ns (map parse vs))]
+    [`(get ,rec ,n)
+     (GetE (parse rec) n)]
+    [`(set ,rec ,n ,val)
+     (SetE (parse rec) n (parse val))]
+    [`(lambda ([,var : ,ty]) ,body)
+     (LamE var (parse-type ty) (parse body))]
+    [`(let ([,var : ,ty ,val]) ,body)
+     (AppE (LamE var (parse-type ty) (parse body)) (parse val))]
+    [`(,fun ,arg) (AppE (parse fun) (parse arg))]
+    [else (error 'parse "invalid expression")]))
+
+(define (parse-type t)
+  (match t
+    ['num (NumT)]
+    ['bool (BoolT)]
+    [`((,ns : ,ts) ...) (RecordT ns (map parse-type ts))]
+    [`(,tyarg -> ,tyres) (ArrowT (parse-type tyarg) (parse-type tyres))]
+    [else (error 'parse-type "invalid type")]))
+
+;; Type Checker
+
+(define (typecheck-nums l r tenv)
+  (match* ((typecheck l tenv) (typecheck r tenv))
+    [((NumT) (NumT)) (NumT)]
+    [(_ _) (error 'typecheck "not number")]))
+
+(define (record-find n ns ts)
+  (cond [(empty? ns) (error 'find "can not find")]
+        [else (if (symbol=? n (first ns)) (first ts)
+                  (record-find n (rest ns) (rest ts)))]))
+
+;; is t1 a subtype of t2?
+(define (subtype? t1 t2)
+  (match* (t1 t2)
+    [((NumT) (NumT)) #t]
+    [((NumT) _) #f]
+    [((BoolT) (BoolT)) #t]
+    [((BoolT) _) #f]
+    [((ArrowT l1 r1) (ArrowT l2 r2))
+     (and (subtype? l2 l1)   ;contra-variant
+          (subtype? r1 r2))] ;co-variant
+    [((ArrowT _ _) _) #f]
+    [((RecordT ns1 ts1) (RecordT ns2 ts2))
+     ; every field in ns2 must be in ns1 and types in
+     ; ts1 should be a subtype of the one in ts2
+     (andmap (λ (n) (and (member n ns1)
+                         (subtype? (record-find n ns1 ts1)
+                                   (record-find n ns2 ts2))))
+             ns2)]
+    [((RecordT _ _) _) #f]
+    [(_ _) #f]))
+
+(define (typecheck exp tenv)
+  (match exp
+    [(NumE n) (NumT)]
+    [(BoolE b) (BoolT)]
+    [(PlusE l r) (typecheck-nums l r tenv)]
+    [(MultE l r) (typecheck-nums l r tenv)]
+    [(IdE n) (type-lookup n tenv)]
+    [(IfE cnd thn els)
+     (if (BoolT? (typecheck cnd tenv))
+         (let ([thn-type (typecheck thn tenv)]
+               [els-type (typecheck els tenv)])
+           (if (equal? thn-type els-type) thn-type
+               (error 'typecheck "types of branches not agree")))
+         (error 'typecheck "not a boolean"))]
+    [(LamE arg arg-type body)
+     (ArrowT arg-type (typecheck body (ext-tenv (TypeBinding arg arg-type) tenv)))]
+    [(AppE fun arg)
+     (match (typecheck fun tenv)
+       [(ArrowT atype rtype)
+        (if (subtype? (typecheck arg tenv) atype) rtype
+            (error 'type "argument types not agree"))]
+       [_ (error 'typecheck "not a function")])]
+    [(RecordE ns es)
+     (RecordT ns (map (λ (e) (typecheck e tenv)) es))]
+    [(GetE rec n)
+     (match (typecheck rec tenv)
+       [(RecordT ns ts) (record-find n ns ts)]
+       [else (error 'typecheck "not a record")])]
+    [(SetE rec n val)
+     (define rec-type (typecheck rec tenv))
+     (match rec-type
+       [(RecordT ns ts)
+        (define field-type (record-find n ns ts))
+        (if (subtype? (typecheck val tenv) field-type)
+            rec-type
+            (error 'typecheck "should be subtype of field"))]
+       [_ (error 'typecheck "not a record")])]))
+
+;; Interpreter
+
+(define (interp expr env)
+  (match expr
+    [(IdE x) (lookup x env)]
+    [(NumE n) (NumV n)]
+    [(BoolE b) (BoolV b)]
+    [(PlusE l r) (NumV (+ (NumV-n (interp l env))
+                          (NumV-n (interp r env))))]
+    [(MultE l r) (NumV (* (NumV-n (interp l env))
+                          (NumV-n (interp r env))))]
+    [(LamE arg at body) (ClosureV arg body env)]
+    [(AppE fun arg)
+     (match (interp fun env)
+       [(ClosureV n body env*)
+        (interp body (ext-env (Binding n (interp arg env)) env*))]
+       [else (error 'interp "not a function")])]
+    [(RecordE ns es)
+     (RecordV ns (map (λ (e) (box (interp e env))) es))]
+    [(GetE rec n)
+     (define rec-v (interp rec env))
+     (unbox (record-find n (RecordV-ns rec-v) (RecordV-vs rec-v)))]
+    [(SetE rec n v)
+     (define rec-v (interp rec env))
+     (set-box! (record-find n (RecordV-ns rec-v) (RecordV-vs rec-v))
+               (interp v env))
+     rec-v]))
+
+(define mt-env empty)
+(define mt-tenv empty)
+
+(define (run prog)
+  (define prog* (parse prog))
+  (typecheck prog* mt-tenv)
+  (interp prog* mt-env))
+
+;; Tests
+
+(module+ test
+  (check-equal? (parse-type '{{x : num} {y : num}})
+                (RecordT '(x y) (list (NumT) (NumT))))
+  (check-equal? (parse-type '{{x : {num -> num}}
+                              {y : {bool -> bool}}})
+                (RecordT '(x y) (list (ArrowT (NumT) (NumT))
+                                      (ArrowT (BoolT) (BoolT)))))
+  
+  (check-equal? (parse '{let {[r : {{x : num}} {rec {x : 3}}]}
+                          {get r x}})
+                (AppE
+                 (LamE 'r (RecordT '(x) (list (NumT))) (GetE (IdE 'r) 'x))
+                 (RecordE '(x) (list (NumE 3)))))
+
+  (check-equal? (typecheck (parse '{let {[r : {{x : num}} {rec {x : 3}}]}
+                                     {get r x}}) mt-tenv)
+                (NumT))
+  
+  (check-true (subtype? (parse-type '{{x : num} {y : num}})
+                        (parse-type '{{x : num}})))
+  
+  (check-true (subtype? (parse-type '{{{x : num}} -> {{x : num} {y : num}}})
+                        (parse-type '{{{x : num} {y : num}} -> {{x : num}}})))
+
+  (check-equal? (typecheck (parse '{{lambda {[r : {{x : num}}]} {+ {get r x} 1}}
+                                    {rec {x : 3} {y : 4}}})
+                           mt-tenv)
+                (NumT))
+
+  (check-equal? (typecheck (parse '{{lambda {[r : {{x : num}}]} {+ {get r x} 1}}
+                                    {rec {y : true} {x : 4}}})
+                           mt-tenv)
+                (NumT))
+
+  (check-equal? (interp (parse '{let {[r : {{x : num}} {rec {x : 3}}]}
+                                  {get r x}}) mt-env)
+                (NumV 3))
+
+  (check-equal? (interp (parse '{{lambda {[r : {{x : num}}]} {+ {get r x} 1}}
+                                 {rec {x : 3} {y : 4}}})
+                        mt-env)
+                (NumV 4))
+  
+  (check-equal? (interp (parse '{{lambda {[r1 : {{x : num}}]}
+                                   {let {[r2 : {{x : num}}
+                                             {rec {x : 3}}]}
+                                     {set r2 x {+ {get r1 x} {get r2 x}}}}}
+                                 {rec {x : 4}}})
+                        mt-env)
+                (RecordV '(x) (list (box (NumV 7)))))
+
+  )
---
+++ systemf.rkt
@@ -0,0 +1,272 @@
+#lang racket
+
+(require rackunit)
+
+;; Implementation of System F
+
+(struct NumV (n) #:transparent)
+(struct BoolV (b) #:transparent)
+(struct ClosureV (arg body env) #:transparent)
+(struct PolyV (body env) #:transparent)
+
+(struct NumE (n) #:transparent)
+(struct BoolE (b) #:transparent)
+(struct IdE (id) #:transparent)
+(struct PlusE (l r) #:transparent)
+(struct MultE (l r) #:transparent)
+(struct LamE (arg arg-type body) #:transparent)
+(struct AppE (fun arg) #:transparent)
+(struct TyLamE (arg body) #:transparent)
+(struct TyAppE (tyfun tyarg) #:transparent)
+
+(struct NumT () #:transparent)
+(struct BoolT () #:transparent)
+(struct FunT (arg result) #:transparent)
+(struct VarT (name) #:transparent)
+(struct ForallT (name tbody) #:transparent)
+
+;; Environment & Type Environment
+
+(define (make-lookup error-hint isa? name-of val-of)
+  (λ (name vals)
+    (cond [(empty? vals) (error error-hint "free variable")]
+          [(and (isa? (first vals))
+                (equal? name (name-of (first vals))))
+           (val-of (first vals))]
+          [else ((make-lookup error-hint isa? name-of val-of) name (rest vals))])))
+
+(struct Binding (name val) #:transparent)
+(define lookup (make-lookup 'lookup Binding? Binding-name Binding-val))
+(define ext-env cons)
+
+(struct TypeVar (id) #:transparent)
+(struct TypeBinding (name type) #:transparent)
+(define type-lookup (make-lookup 'type-lookup TypeBinding? TypeBinding-name TypeBinding-type))
+(define type-var-lookup (make-lookup 'type-var-lookup TypeVar? TypeVar-id TypeVar-id))
+(define ext-tenv cons)
+
+;; Parser
+
+(define (parse s)
+  (match s
+    [(? number? x) (NumE x)]
+    ['true (BoolE #t)]
+    ['false (BoolE #f)]
+    [(? symbol? x) (IdE x)]
+    [`(+ ,l ,r) (PlusE (parse l) (parse r))]
+    [`(* ,l ,r) (MultE (parse l) (parse r))]
+    [`(let ([,var : ,ty ,val]) ,body)
+     (AppE (LamE var (parse-type ty) (parse body)) (parse val))]
+    [`(lambda ([,var : ,ty]) ,body)
+     (LamE var (parse-type ty) (parse body))]
+    [`(LAMBDA [,tvar] ,body) (TyLamE tvar (parse body))]
+    [`(@ ,tyfun ,tyarg) (TyAppE (parse tyfun) (parse-type tyarg))]
+    [`(,fun ,arg) (AppE (parse fun) (parse arg))]
+    [else (error 'parse "invalid expression")]))
+
+(define (parse-type t)
+  (match t
+    ['num (NumT)]
+    ['bool (BoolT)]
+    [(? symbol? tvar) (VarT tvar)]
+    [`(,tyarg -> ,tyres) (FunT (parse-type tyarg) (parse-type tyres))]
+    [`(forall (,tvar) ,t) (ForallT tvar (parse-type t))]
+    [else (error 'parse-type "invalid type")]))
+
+;; Type Checker
+
+(define (type-error e ty)
+  (error 'type-error "~a should has type: ~a" e ty))
+
+(define (type-check e tenv)
+  (match e
+    [(NumE n) (NumT)]
+    [(BoolE b) (BoolT)]
+    [(IdE n) (type-lookup n tenv)]
+    [(PlusE l r) (type-check-nums l r tenv)]
+    [(MultE l r) (type-check-nums l r tenv)]
+    [(LamE arg-name arg-type body)
+     (type-var-check arg-type tenv)
+     (FunT arg-type (type-check body
+                                (ext-tenv (TypeBinding arg-name arg-type) tenv)))]
+    [(AppE fun arg)
+     (match (type-check fun tenv)
+       [(FunT arg-type res-type)
+        (if (equal? arg-type (type-check arg tenv))
+            res-type
+            (type-error arg arg-type))]
+       [else (type-error fun "function")])]
+    [(TyLamE n body)
+     (ForallT n (type-check body (ext-tenv (TypeVar n) tenv)))]
+    [(TyAppE tyfun tyarg)
+     (type-var-check tyarg tenv)
+     (match (type-check tyfun tenv)
+       [(ForallT n body) (type-subst n tyarg body)]
+       [else (type-error tyfun "polymorphic function")])]))
+
+(define (type-check-nums l r tenv)
+  (match* ((type-check l tenv)
+           (type-check r tenv))
+    [((NumT) (NumT)) (NumT)]
+    [((NumT) _) (type-error r (NumT))]
+    [(_ _) (type-error l (NumT))]))
+
+(define (type-var-check arg-type tenv)
+  (match arg-type
+    [(NumT) (values)]
+    [(BoolT) (values)]
+    [(FunT arg res)
+     (type-var-check arg tenv) (type-var-check res tenv)
+     (values)]
+    [(VarT id) (type-var-lookup id tenv) (values)]
+    [(ForallT id ty) (type-var-check ty (ext-tenv (TypeVar id) tenv))]))
+
+(define (type-subst what for in)
+  (match in
+    [(NumT) (NumT)]
+    [(BoolT) (BoolT)]
+    [(FunT arg res) (FunT (type-subst what for arg)
+                          (type-subst what for res))]
+    [(VarT n) (if (equal? what n) for in)]
+    [(ForallT n body)
+     (cond [(equal? what n) (ForallT n body)]
+           [(free-type-var? n for)
+            (define new-n (gen-name n 1 for body))
+            (define new-body (type-subst n (VarT new-n) body))
+            (type-subst what for (ForallT new-n new-body))]
+           [else (ForallT n (type-subst what for body))])]))
+
+(define (gen-name n i for body)
+  (let ([new-n (string->symbol (string-append (symbol->string n)
+                                              (number->string i)))])
+    (if (or (free-type-var? new-n for)
+            (free-type-var? new-n body))
+        (gen-name n (+ i 1) for body)
+        new-n)))
+
+(define (free-type-var? n ty)
+  (match ty
+    [(NumT) #f]
+    [(BoolT) #f]
+    [(FunT a r) (or (free-type-var? n a)
+                    (free-type-var? r a))]
+    [(VarT n^) (equal? n^ n)]
+    [(ForallT n^ body)
+     (if (equal? n n^) #f (free-type-var? n body))]))
+
+;; Interpreter
+
+(define (interp expr env)
+  (match expr
+    [(IdE x) (lookup x env)]
+    [(NumE n) (NumV n)]
+    [(BoolE b) (BoolV b)]
+    [(PlusE l r) (NumV (+ (NumV-n (interp l env))
+                          (NumV-n (interp r env))))]
+    [(MultE l r) (NumV (* (NumV-n (interp l env))
+                          (NumV-n (interp r env))))]
+    [(LamE n t body) (ClosureV n body env)]
+    [(AppE fun arg) (match (interp fun env)
+                      [(ClosureV n body env*)
+                       (interp body (ext-env (Binding n (interp arg env)) env*))]
+                      [else (error 'interp "not a function")])]
+    [(TyLamE n body) (PolyV body env)]
+    [(TyAppE tyfun tyarg)
+     (match (interp tyfun env)
+       [(PolyV body env*) (interp body env*)]
+       [else (error 'interp "not a polymorphic function")])]))
+
+(define mt-env empty)
+(define mt-tenv empty)
+(define (run prog)
+  (define prog* (parse prog))
+  (type-check prog* mt-tenv)
+  (interp prog* mt-env))
+
+;; Test
+
+(check-equal? (parse-type '{a -> a})
+              (FunT (VarT 'a) (VarT 'a)))
+
+(check-equal? (parse-type '{forall {a} {a -> a}})
+              (ForallT 'a (FunT (VarT 'a) (VarT 'a))))
+              
+(check-equal? (parse '{let {[id : {forall {a} {a -> a}}
+                                [LAMBDA [a] {lambda {[x : a]} x}]]}
+                        {+ {[@ id num] 1} {[@ id num] 2}}})
+              (AppE
+               (LamE
+                'id
+                (ForallT 'a (FunT (VarT 'a) (VarT 'a)))
+                (PlusE
+                 (AppE (TyAppE (IdE 'id) (NumT)) (NumE 1))
+                 (AppE (TyAppE (IdE 'id) (NumT)) (NumE 2))))
+               (TyLamE 'a (LamE 'x (VarT 'a) (IdE 'x)))))
+
+(check-equal? (type-check (parse '{let {[id : {forall {a} {a -> a}}
+                                            [LAMBDA [a] {lambda {[x : a]} x}]]}
+                                    {+ {[@ id num] 1} {[@ id num] 2}}})
+                          mt-tenv)
+              (NumT))
+
+(check-equal? (parse '{let {[x : num 4]}
+                        {let {[y : num 5]}
+                          {{{lambda {[x : num]}
+                              {lambda {[y : num]}
+                                {+ x y}}} x} y}}})
+              (AppE (LamE 'x (NumT)
+                          (AppE (LamE 'y (NumT)
+                                      (AppE (AppE (LamE 'x (NumT)
+                                                        (LamE 'y (NumT) (PlusE (IdE 'x) (IdE 'y)))) (IdE 'x))
+                                            (IdE 'y))) (NumE 5))) (NumE 4)))
+
+(check-equal? (type-check (parse '{let {[x : num 4]}
+                                    {let {[y : num 5]}
+                                      {{{lambda {[x : num]}
+                                          {lambda {[y : num]}
+                                            {+ x y}}} x} y}}}) mt-tenv)
+              (NumT))
+
+(check-equal? (run '{let {[x : num 4]}
+                      {let {[y : num 5]}
+                        {{{lambda {[x : num]}
+                            {lambda {[y : num]}
+                              {+ x y}}} x} y}}})
+              (NumV 9))
+
+(check-equal? (run '{let {[id : {forall {a} {a -> a}}
+                              [LAMBDA [a] {lambda {[x : a]} x}]]}
+                      {+ {[@ id num] 1} {{[@ id {num -> num}] {lambda {[x : num]} x}} 2}}})
+              (NumV 3))
+
+(check-equal? (type-check
+               (parse '{let {[f : {forall {a} {a -> {forall {b} {{a -> b} -> b}}}}
+                                [LAMBDA [a] {lambda {[x : a]}
+                                              [LAMBDA [b] {lambda {[g : {a -> b}]} {g x}}]}]]}
+                         {[@ {[@ f num] 3} bool] {lambda {[x : num]} true}}})
+               mt-tenv)
+              (BoolT))
+
+(check-equal? (run '{let {[f : {forall {a} {a -> {forall {b} {{a -> b} -> b}}}}
+                             [LAMBDA [a] {lambda {[x : a]}
+                                           [LAMBDA [b] {lambda {[g : {a -> b}]} {g x}}]}]]}
+                      {[@ {[@ f num] 3} bool] {lambda {[x : num]} true}}})
+              (BoolV #t))
+
+(define Bool '{forall [a] {a -> {a -> a}}})
+(define True '{LAMBDA [a] {lambda {[x : a]} {lambda {[y : a]} x}}})
+(define False '{LAMBDA [a] {lambda {[x : a]} {lambda {[y : a]} y}}})
+(define And `{lambda {[x : ,Bool]} {lambda {[y : ,Bool]} {{[@ x ,Bool] y} ,False}}})
+(define Bool->Num `{lambda {[x : ,Bool]} {{[@ x num] 1} 0}})
+
+(check-equal? (run `{let {[t : ,Bool ,True]}
+                {let {[f : ,Bool ,False]}
+                {let {[and : {,Bool -> {,Bool -> ,Bool}} ,And]}
+                {,Bool->Num {{and t} f}}}}})
+              (NumV 0))
+
+(check-equal? (run `{let {[t : ,Bool ,True]}
+                {let {[f : ,Bool ,False]}
+                {let {[and : {,Bool -> {,Bool -> ,Bool}} ,And]}
+                {,Bool->Num {{and t} t}}}}})
+              (NumV 1))
